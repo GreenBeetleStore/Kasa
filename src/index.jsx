b1d8319501c956a5654a70ab3b5f7818e250209b
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-30 14:47:42 +0100

    Degradació de versió React18 a React17

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/style.css'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
    <BrowserRouter>
       <React.StrictMode>
          <App />
       </React.StrictMode>
    </BrowserRouter>
+
+document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
