f5f46adfa01fc7dd3663447f30c5bb5dbd556a35
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-30 16:50:10 +0100

    renombrar APropos

--- src/pages/A-Propos.jsx
+++
@@ -1,5 +0,0 @@
-// A-Propos · src/pages/A-Propos.jsx
-
-// import Header from '../pages/Header'
-
-// Header()
---
+++ src/pages/APropos.jsx
@@ -0,0 +1,14 @@
+// A-Propos · src/pages/A-Propos.jsx
+
+import React from 'react';
+import '../styles/style.css'
+
+const APropos = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default APropos;
